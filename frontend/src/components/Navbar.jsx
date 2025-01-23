2cffa12780246e7702df695ea65b933ec6076060
Aviral2403 <140821586+Aviral2403@users.noreply.github.com>
2025-01-23 20:42:24 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { useContext, useState } from "react";
 import Menu from "./Menu";
@@ -13,30 +13,53 @@ const Navbar = () => {
 
   const { user } = useContext(UserContext);
 
-
-  
   return (
     <div className="sticky top-0 z-50 flex items-center justify-between px-6 md:px-[100px] py-4 bg-gray-900">
-      <div className="flex flex-row gap-2">
+      <div className="flex flex-row gap-2 justify-between items-center w-full">
         <Link to="/" className="flex items-center space-x-2">
           <img src="/NavLogo.png" alt="Logo" width={50} height={60} />
           <h1 className="text-lg font-cursive text-shadow-lg text-gray-100 cursor-pointer font-semibold">
             ScribblyPosts
           </h1>
         </Link>
+        <Link to='/'>
+          <h1 className="text-sm text-gray-100 cursor-pointer hover:bg-[#bdc2e7] hover:text-gray-900 p-1 rounded-md text-center items-center block md:hidden">
+            Home
+          </h1>
+        </Link>
       </div>
       <div className="hidden md:flex items-center justify-center space-x-2 md:space-x-4 text-gray-100 ">
-        <Link to="/" className="hover:bg-[#bdc2e7] hover:text-gray-900 p-1 rounded-md">Home</Link>
+        <Link
+          to="/"
+          className="hover:bg-[#bdc2e7] hover:text-gray-900 p-1 rounded-md"
+        >
+          Home
+        </Link>
 
-        <Link to="/about" className="hover:bg-[#bdc2e7] hover:text-gray-900 p-1 rounded-md">About</Link>
+        <Link
+          to="/about"
+          className="hover:bg-[#bdc2e7] hover:text-gray-900 p-1 rounded-md"
+        >
+          About
+        </Link>
 
         {user ? (
           <h3>
-            <Link to="/write" className="hover:bg-[#bdc2e7] hover:text-gray-900 p-1 rounded-md">Write</Link>
+            <Link
+              to="/write"
+              className="hover:bg-[#bdc2e7] hover:text-gray-900 p-1 rounded-md"
+            >
+              Write
+            </Link>
           </h3>
         ) : (
           <h3>
-            <Link to="/login" className="hover:bg-[#bdc2e7] hover:text-gray-900 p-1 rounded-md">Login</Link>
+            <Link
+              to="/login"
+              className="hover:bg-[#bdc2e7] hover:text-gray-900 p-1 rounded-md"
+            >
+              Login
+            </Link>
           </h3>
         )}
         <div className="hidden md:flex items-center justify-center  text-gray-100"></div>
@@ -50,7 +73,12 @@ const Navbar = () => {
         ) : (
           <>
             <h3>
-              <Link to="/register" className="hover:bg-[#bdc2e7] hover:text-gray-900 p-1 rounded-md">Register</Link>
+              <Link
+                to="/register"
+                className="hover:bg-[#bdc2e7] hover:text-gray-900 p-1 rounded-md"
+              >
+                Register
+              </Link>
             </h3>
           </>
         )}
