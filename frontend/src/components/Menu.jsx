7df225ad02c83fa42b975192d6bb20b7f9c4c8c1
Aviral2403 <140821586+Aviral2403@users.noreply.github.com>
2025-01-23 20:42:52 +0530

    Update Menu.jsx

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -23,7 +23,6 @@ const Menu = () => {
         <div className="bg-gray-900 bg-opacity-80 w-[200px] z-10 flex flex-col items-start absolute top-12 right-6 md:right-32 rounded-md p-4 space-y-4">
             {!user && <h3 className="text-white text-sm hover:bg-[#bdc2e7] cursor-pointer hover:text-gray-900 rounded-md p-1"><Link to="/login">Login</Link></h3>}
             {!user && <h3 className="text-white text-sm hover:bg-[#bdc2e7] cursor-pointer hover:text-gray-900 rounded-md p-1"><Link to="/register">Register</Link></h3>}
-            {user && <h3 className="md:hidden text-white text-sm hover:bg-[#bdc2e7] cursor-pointer hover:text-gray-900 rounded-md p-1"><Link to="/">Home</Link></h3>}
             {user && <h3 className="md:hidden text-white text-sm hover:bg-[#bdc2e7] cursor-pointer hover:text-gray-900 rounded-md p-1"><Link to="/about">About</Link></h3>}
             {user && <h3 className="md:hidden text-white text-sm hover:bg-[#bdc2e7] cursor-pointer hover:text-gray-900 rounded-md p-1"><Link to="/write">Write</Link></h3>}
             {user && <h3 className="text-white text-sm hover:bg-[#bdc2e7] cursor-pointer hover:text-gray-900 rounded-md p-1"><Link to={"/myblogs/" + user._id}>My blogs</Link></h3>}
